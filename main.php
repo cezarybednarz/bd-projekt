e19dd1b14db5fdbacd7522e11959b0fcacdb3969
cezary <cezary.bednarz@gmail.com>
2020-01-27 20:34:56 +0100

    xd

--- main.php
+++ main.php
@@ -68,7 +68,7 @@ th {
     <th>replies</th>
   </tr>
   <?PHP
-    $stmt = oci_parse($conn, "SELECT * FROM TWEET");
+    $stmt = oci_parse($conn, "SELECT * FROM TEST");
     oci_execute($stmt, OCI_NO_AUTO_COMMIT);
     while (($row = oci_fetch_array($stmt, OCI_BOTH))) {
       echo "<tr>";
