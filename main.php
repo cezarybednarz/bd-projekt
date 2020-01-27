2d8fd71f84c1ec30eb7928bee75832428397e727
cezary <cezary.bednarz@gmail.com>
2020-01-27 16:20:28 +0100

    xd

--- main.php
+++ main.php
@@ -9,43 +9,43 @@
 
 <?PHP // Wchodzimy do PHP
 
-    ////////////////////////////////// 
-    // Tworzenie ciasteczka sesyjnego.
-    session_start(); 
-    // Zapisanie loginu i hasla w ciasteczku sesyjnym.
-    // mozna odkomentowac zeby sie logowac na oracla za kazdym razem
-    //$_SESSION['LOGIN'] = $_POST['LOGN'];
-    //$_SESSION['PASS'] = $_POST['PASW'];
-    /////////////////////////////////// 
+  ////////////////////////////////// 
+  // Tworzenie ciasteczka sesyjnego.
+  session_start(); 
+  // Zapisanie loginu i hasla w ciasteczku sesyjnym.
+  // mozna odkomentowac zeby sie logowac na oracla za kazdym razem
+  //$_SESSION['LOGIN'] = $_POST['LOGN'];
+  //$_SESSION['PASS'] = $_POST['PASW'];
+  /////////////////////////////////// 
+  
+  $_SESSION['LOGIN'] = 'cb406099';
+  $_SESSION['PASS'] = 'xxx';
+  
+  // Nawiazywanie polaczenia z baza danych; login i haslo do studenckiego oracla; serwer bazodanowy jest domyslny. 
+  $conn = oci_connect($_SESSION['LOGIN'] , $_SESSION['PASS']);
+  
+  if (!$conn) {
     
-    $_SESSION['LOGIN'] = 'cb406099';
-    $_SESSION['PASS'] = 'xxx';
-    
-    // Nawiazywanie polaczenia z baza danych; login i haslo do studenckiego oracla; serwer bazodanowy jest domyslny. 
-    $conn = oci_connect($_SESSION['LOGIN'] , $_SESSION['PASS']);
-    
-    if (!$conn) {
-      
-      echo "oci_connect failed\n";
-    	$e = oci_error();
-	    echo $e['message'];
-    }
+    echo "oci_connect failed\n";
+  	$e = oci_error();
+    echo $e['message'];
+  }
 
-    // Tworzenie wyrazenia SQL-owego
-    $stmt = oci_parse($conn, "SELECT * FROM test");
+  // Tworzenie wyrazenia SQL-owego
+  $stmt = oci_parse($conn, "SELECT * FROM test");
 
-    // Wykonywanie wyrazenia SQL-owego
-    oci_execute($stmt, OCI_NO_AUTO_COMMIT);
+  // Wykonywanie wyrazenia SQL-owego
+  oci_execute($stmt, OCI_NO_AUTO_COMMIT);
 
-    // OCI_BOTH sprawia, tablica jest zarowno asocjacyjna, jak i zwykla
-    while (($row = oci_fetch_array($stmt, OCI_BOTH))) {
-	// Use uppercase column names for the associative array indices and
-        // numbers for the ordinary array indices.
-        echo "<BR><A HREF=\"doktoranci.php?id=" . $row['ID'] . "\">" . $row[1] . " " . $row['NAZWISKO'] . "</A><BR>\n";
-    }
-    
-    // Jesli modyfikujemy, to trzeba zrobic COMMIT:
-    // oci_commit($conn);
+  // OCI_BOTH sprawia, tablica jest zarowno asocjacyjna, jak i zwykla
+  while (($row = oci_fetch_array($stmt, OCI_BOTH))) {
+// Use uppercase column names for the associative array indices and
+      // numbers for the ordinary array indices.
+      echo "<BR><A HREF=\"doktoranci.php?id=" . $row['ID'] . "\">" . $row['val_a'] . " " . $row['val_b'] . "</A><BR>\n";
+  }
+  
+  // Jesli modyfikujemy, to trzeba zrobic COMMIT:
+  // oci_commit($conn);
 ?>
 
 </BODY>
