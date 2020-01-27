ade7f3ff379e7baed112fef9c2622a6bbdd25295
cezary <cezary.bednarz@gmail.com>
2020-01-27 22:41:54 +0100

    xd

--- main.php
+++ main.php
@@ -82,7 +82,7 @@ th {
     oci_execute($stmt, OCI_NO_AUTO_COMMIT);
     while (($row = oci_fetch_array($stmt, OCI_BOTH))) {
       echo "<tr>";
-      echo "<td>" . $row['ID'] . "</td>";
+      echo "<td><a href="twitter.php?id=" . $row['ID'] . ">" . $row['ID'] . "</a></td>";
       echo "<td>" . $row['VAL_A'] . "</td>";
       echo "<td>" . $row['VAL_B'] . "</td>";
       echo "</tr>";
