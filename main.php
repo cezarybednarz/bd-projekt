7c4ce3cdd1d0022b19c41f5f5beddbf77dee292c
cezary <cezary.bednarz@gmail.com>
2020-01-27 16:01:36 +0100

    xd

--- main.php
+++ main.php
@@ -13,9 +13,13 @@
     // Tworzenie ciasteczka sesyjnego.
     session_start(); 
     // Zapisanie loginu i hasla w ciasteczku sesyjnym.
-    $_SESSION['LOGIN'] = $_POST['LOGN'];
-    $_SESSION['PASS'] = $_POST['PASW'];
+    // mozna odkomentowac zeby sie logowac na oracla za kazdym razem
+    //$_SESSION['LOGIN'] = $_POST['LOGN'];
+    //$_SESSION['PASS'] = $_POST['PASW'];
     /////////////////////////////////// 
+    
+    $_SESSION['LOGIN'] = "cb406099";
+    $_SESSION['PASS'] = "xxx";
     
     // Nawiazywanie polaczenia z baza danych; login i haslo do studenckiego oracla; serwer bazodanowy jest domyslny. 
     $conn = oci_connect($_POST['LOGN'],$_POST['PASW']);
