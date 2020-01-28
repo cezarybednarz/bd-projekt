20eff7f0665ae2cecabe5e05fcb2246e27ba54f1
cezary <cezary.bednarz@gmail.com>
2020-01-28 23:15:31 +0100

    xd

---
+++ admin.php
@@ -0,0 +1,75 @@
+<HTML>
+
+
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+<link rel='stylesheet' type='text/css' href='//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>
+
+<head>
+<style>
+table {
+  border-collapse: collapse;
+  width: 80%;
+}
+
+th, td {
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even){background-color: #f2f2f2}
+
+th {
+  background-color: #4267B2;
+  color: white;
+}
+</style>
+
+</head>
+
+<HEAD>
+    <TITLE> Sekcja admina </TITLE>
+</HEAD>
+
+<BODY>
+
+<center>
+
+<h2> Dodawanie/usuwanie JSONów </h2>
+
+<center><a href="http://students.mimuw.edu.pl/~cb406099/bd-projekt">wróć do strony głównej</a></br>
+
+<center><a href="javascript:history.back()">wróć do poprzedniej strony</a></br>
+
+
+<?PHP
+
+  session_start(); 
+  //$_SESSION['LOGIN'] = $_POST['LOGN'];
+  //$_SESSION['PASS'] = $_POST['PASW'];
+  $_SESSION['LOGIN'] = 'cb406099';
+  $_SESSION['PASS'] = 'xxx';
+  
+  $conn = oci_connect($_SESSION['LOGIN'] , $_SESSION['PASS']);
+  
+  if (!$conn) {
+    
+    echo "oci_connect failed\n";
+  	$e = oci_error();
+    echo $e['message'];
+  }
+  
+  echo '        <form action="dodajdruzyne2.php" method="post"> 
+            Nazwa drużyny : <input type="text" name="teamName" size="40" length="40" value="Nazwa druzyny"><BR> 
+            <input type="submit" name="submit" value="Zatwierd"> 
+            <input type="reset" name="reset" value="Wyczyć"> 
+        </form> ';}
+  
+  
+  
+?>
+</table>
+</center>
+
+</BODY>
+</HTML>
+
