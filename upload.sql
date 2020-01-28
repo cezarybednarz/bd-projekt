76ae6cd2b5c0e93358e276735bfa22391fb2c909
cezary <cezary.bednarz@gmail.com>
2020-01-28 20:05:56 +0100

    xd

--- upload.sql
+++ upload.sql
@@ -1,15 +1,14 @@
 
+SET SERVEROUTPUT ON;
 
 
-CREATE OR REPLACE FUNCTION parse_tweet(char_json IN VARCHAR2) IS
+CREATE OR REPLACE FUNCTION parse_tweet(char_json IN VARCHAR2) 
+RETURN VARCHAR2
+IS
   json JSON_OBJECT_T;
-  search_metadata JSON_OBJECT_T;
 BEGIN
   json := JSON_OBJECT.parse(char_json);
-  FOR tweet IN json.get_object('statuses')
-  LOOP
-    dbms_output.put_line(tweet.get_object('id_str').to_string);
-  END LOOP;
 END;
 /
   
+SHOW ERRORS;
