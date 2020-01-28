ec7e896d986d9f57552f8b72b4c4b6b3f6503eb1
cezary <cezary.bednarz@gmail.com>
2020-01-28 19:36:19 +0100

    xd

--- upload.sql
+++ upload.sql
@@ -1,5 +1,15 @@
-CREATE OR REPLACE FUNCTION parse_json(char_json IN VARCHAR2) IS
+
+
+
+CREATE OR REPLACE FUNCTION parse_tweet(char_json IN VARCHAR2) IS
   json JSON_OBJECT_T;
+  search_metadata JSON_OBJECT_T;
 BEGIN
-  json := JSON_OBJECT_T.parse(char_json);
+  json := JSON_OBJECT.parse(char_json);
+  FOR tweet IN json.get_object('statuses')
+  LOOP
+    dbms_output.put_line(tweet.get_object('id_str').to_string);
+  END LOOP;
+END;
+/
   
