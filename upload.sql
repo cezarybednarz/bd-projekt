a896add596c39e17d5df1222d9f2756a85f5885b
cezary <cezary.bednarz@gmail.com>
2020-01-28 02:50:44 +0100

    xd

--- upload.sql
+++ upload.sql
@@ -1,6 +1,6 @@
-CREATE OR REPLACE PROCEDURE greetings 
-AS 
-BEGIN 
-   dbms_output.put_line('Hello World!'); 
-END; 
+CREATE OR REPLACE PROCEDURE upload_json
+AS
+BEGIN
+  dbms_output.put_line('xddd');
+END;
 /
