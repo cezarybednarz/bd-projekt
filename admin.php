0cac491e1d4c96bbe543fcd2548e8e6f688d6669
cezary <cezary.bednarz@gmail.com>
2020-01-29 01:53:59 +0100

    xd

--- admin.php
+++ admin.php
@@ -109,14 +109,7 @@ th {
       oci_bind_by_name($stmt, ':id', $tweet->id);
       oci_bind_by_name($stmt, ':user_id', $usr->id);
       oci_bind_by_name($stmt, ':text', $tweet->text);
-      /*
-      if($tweet->retweeted) {    
-        oci_bind_by_name($stmt, ':retweeted', 1);
-      }
-      else {      
-      */
-        oci_bind_by_name($stmt, ':retweeted', 0);
-      
+      oci_bind_by_name($stmt, ':retweeted', $tweet->retweeted);
       oci_bind_by_name($stmt, ':favourite_count', $tweet->favourite_count);
       oci_bind_by_name($stmt, ':retweet_count', $tweet->retweet_count);
       oci_bind_by_name($stmt, ':reply_count', $tweet->reply_count);
