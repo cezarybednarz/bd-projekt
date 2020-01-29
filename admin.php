4ef26cb7f903d8295a1894f947cffaada41e8fde
cezary <cezary.bednarz@gmail.com>
2020-01-29 03:41:50 +0100

    xd

--- admin.php
+++ admin.php
@@ -245,7 +245,6 @@ th {
   
   $stmt = oci_parse($conn, "SELECT DISTINCT query FROM tweet");
   oci_execute($stmt, OCI_NO_AUTO_COMMIT);
-  $row = oci_fetch_array($stmt, OCI_BOTH);
   echo "<b>zapytania z załadowanych plików: </b></br>";
   while (($row = oci_fetch_array($stmt, OCI_BOTH))) {
     echo "<i>" . $row[0] . "</i></br>";
