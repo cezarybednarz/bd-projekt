9b85c7f77aa7d4a3e8d57ed5c2523c5ea36b2fe4
cezary <cezary.bednarz@gmail.com>
2020-02-10 20:48:03 +0100

    xd

--- stats.php
+++ stats.php
@@ -1,8 +1,45 @@
 <!DOCTYPE html>
-<html lang="en-US">
+
+<HTML>
+
+
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+<link rel='stylesheet' type='text/css' href='//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>
+
+<head>
+<style>
+table {
+  border-collapse: collapse;
+  width: 80%;
+}
+
+th, td {
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even){background-color: #f2f2f2}
+
+th {
+  background-color: #4267B2;
+  color: white;
+}
+</style>
+
+</head>
+
+<HEAD>
+    <TITLE> Wykresy </TITLE>
+</HEAD>
+
+<BODY>
+
+<center>
+
+<h2> Wykresy </h2>
+
 <body>
 
-<h1>My Web Page</h1>
 
 <div id="piechart"></div>
 
