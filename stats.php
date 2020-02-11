1e5c52d8b3ecd977a5bd8451d39d21dc1c3e76a0
cezary <cezary.bednarz@gmail.com>
2020-02-11 01:18:58 +0100

    xd

--- stats.php
+++ stats.php
@@ -78,16 +78,19 @@ th {
 </form>
 <?php
   $loaded = [];
-  echo "<p>akutalnie załadowane pliki:</p><br/>";
+  echo "<br/><p>akutalnie załadowane pliki:</p><br>";
+  echo "<table>";
   if(isset($_POST['submit'])){//to run PHP script on submit
     if(!empty($_POST['check_list'])){
     // Loop to store and display values of individual checked checkbox.
       foreach($_POST['check_list'] as $selected) {
-        echo $selected."</br>";
+        echo "<i>" . $selected."</i></br>";
         array_push($loaded, $selected);
       }
     }
   }
+  echo "</table>";
+  
 ?>
 
 
