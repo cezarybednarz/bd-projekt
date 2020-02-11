a78fd737a621dfb0238e3b7fb855bd17c09b268f
cezary <cezary.bednarz@gmail.com>
2020-02-11 02:26:06 +0100

    xd

--- stats.php
+++ stats.php
@@ -207,7 +207,7 @@ th {
           [ new Date(2012, 3, 14), 38024 ],
           <?php
             for($i = 0; $i < $n_date_rows; $i++) {
-              if(in_array($date_rows[FROM_FILE][$i], $loaded) {
+              if(in_array($date_rows[FROM_FILE][$i], $loaded)) {
                 echo "[new Date(" . $date_rows[TIME] . "), 1],";
               }
             }
