958a7710bfd7ba2fc913a19db6880162d1dd33c0
cezary <cezary.bednarz@gmail.com>
2020-02-11 01:00:32 +0100

    xd

--- stats.php
+++ stats.php
@@ -70,12 +70,22 @@ th {
   }
 ?>
 
-
-<input type="checkbox" name="product_id[]" value="1">Product #1<br/>
-<input type="checkbox" name="product_id[]" value="2">Product #2<br/>
-<input type="checkbox" name="product_id[]" value="3">Product #3<br/>
-
-
+<form action="#" method="post">
+<input type="checkbox" name="check_list[]" value="C/C++"><label>C/C++</label><br/>
+<input type="checkbox" name="check_list[]" value="Java"><label>Java</label><br/>
+<input type="checkbox" name="check_list[]" value="PHP"><label>PHP</label><br/>
+<input type="submit" name="submit" value="Submit"/>
+</form>
+<?php
+if(isset($_POST['submit'])){//to run PHP script on submit
+if(!empty($_POST['check_list'])){
+// Loop to store and display values of individual checked checkbox.
+foreach($_POST['check_list'] as $selected){
+echo $selected."</br>";
+}
+}
+}
+?>
 
 
 
