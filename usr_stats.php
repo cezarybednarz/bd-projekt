d779965c98e7c509cd218dda7360873beee03597
cezary <cezary.bednarz@gmail.com>
2020-02-11 04:14:19 +0100

    xd

---
+++ usr_stats.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+
+<HTML>
+
+
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+<link rel='stylesheet' type='text/css' href='//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>
+
+<head>
+<style>
+table {
+  border-collapse: collapse;
+  width: 80%;
+}
+
+th, td {
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even){background-color: #f2f2f2}
+
+th {
+  background-color: #4267B2;
+  color: white;
+}
+</style>
+
+</head>
+
+<HEAD>
+    <TITLE> Wykresy </TITLE>
+</HEAD>
+
+<BODY>
+
+<center>
+
+<h2> Wykresy </h2>
+
+
+<center><a href="http://students.mimuw.edu.pl/~cb406099/bd-projekt">wróć do strony głównej</a></br>
+
+<center><a href="javascript:history.back()">wróć do poprzedniej strony</a></br>
+
+<body>
+
+<?php 
+  
+  session_start(); 
+  $_SESSION['LOGIN'] = 'cb406099';
+  $_SESSION['PASS'] = 'xxx';
+  $conn = oci_connect($_SESSION['LOGIN'] , $_SESSION['PASS']);
+  if (!$conn) {
+    
+    echo "oci_connect failed\n";
+  	$e = oci_error();
+    echo $e['message'];
+  }
+  
+  // pierwszy wykres
+  $stmt = oci_parse($conn, "SELECT statuses_count, followers FROM usr");
+  oci_execute($stmt, OCI_NO_AUTO_COMMIT);
+  $nrows = oci_fetch_all($stmt, $rows);
+?>
+
+
+
+<html>
+  <head>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['statuses count', 'followers'],
+          <?php
+            for($i = 0; $i < $nrows; $i++) {
+              echo "[" . $rows[STATUSES_COUNT][$i] . ", " . $rows[FOLLOWERS][$i] . "],";
+            }
+         ?>	 
+        ]);
+
+        var options = {
+          title: 'Porównanie statuses vs. followers',
+          hAxis: {title: 'Statuses'},
+          vAxis: {title: 'Followers'},
+          legend: 'none'
+        };
+
+        var chart = new google.visualization.ScatterChart(document.getElementById('chart_div'));
+
+        chart.draw(data, options);
+      }
+    </script>
+  </head>
+  <body>
+    <div id="chart_div" style="width: 900px; height: 500px;"></div>
+  </body>
+</html>
+
+
+</body>
+</html>
+
