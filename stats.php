c9ee7c25f1e160cc77bf26b8b7f43b3527cf6c35
cezary <cezary.bednarz@gmail.com>
2020-02-11 02:33:46 +0100

    xd

--- stats.php
+++ stats.php
@@ -73,13 +73,11 @@ th {
   oci_execute($stmt, OCI_NO_AUTO_COMMIT);
   $n_date_rows = oci_fetch_all($stmt, $date_rows);
   
-  echo "<p>".$n_date_rows."</p>";
-  
   for($i = 0; $i < $n_date_rows; $i++) {
-              if(in_array($date_rows[FROM_FILE][$i], $loaded)) {
-                echo "[new Date(" . $date_rows[TIME] . "), 1],";
-              }
-            }
+    if(in_array($date_rows[FROM_FILE][$i], $loaded)) {
+      echo "<p>[new Date(" . $date_rows[TIME][$i] . "), 1],</p>";
+    }
+  }
 ?>
 
 <?php
@@ -214,7 +212,7 @@ th {
           <?php
             for($i = 0; $i < $n_date_rows; $i++) {
               if(in_array($date_rows[FROM_FILE][$i], $loaded)) {
-                echo "[new Date(" . $date_rows[TIME] . "), 1],";
+                echo "[new Date(" . $date_rows[TIME][$i] . "), 1],";
               }
             }
           ?>
